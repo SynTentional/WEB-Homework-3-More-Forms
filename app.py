4f54762c67c6297159c1b18f69c7778823a0177a
Christopher Mullins <cmullins318@gmail.com>
2021-02-19 14:12:07 -0800

    added image filter

--- app.py
+++ app.py
@@ -136,26 +136,30 @@ def image_filter():
     filter_types = filter_types_dict.keys()
 
     if request.method == 'POST':
-        
+       
         # TODO: Get the user's chosen filter type (whichever one they chose in the form) and save
         # as a variable
-        # HINT: remember that we're working with a POST route here so which requests function would you use?
-        filter_type = ''
+        filter_type = request.form.get("filter_type")
         
         # Get the image file submitted by the user
         image = request.files.get('users_image')
 
         # TODO: call `save_image()` on the image & the user's chosen filter type, save the returned
         # value as the new file path
+        save_image(image, filter_type)
 
         # TODO: Call `apply_filter()` on the file path & filter type
 
         image_url = f'./static/images/{image.filename}'
 
+        apply_filter(image_url, filter_type)
+
         context = {
             # TODO: Add context variables here for:
             # - The full list of filter types
             # - The image URL
+            "filter_names": filter_types_dict.keys(),
+            "image_url": image_url
         }
 
         return render_template('image_filter.html', **context)
@@ -163,9 +167,9 @@ def image_filter():
     else: # if it's a GET request
         context = {
             # TODO: Add context variable here for the full list of filter types
+            "filter_names": filter_types_dict.keys()
         }
         return render_template('image_filter.html', **context)
-
 
 ################################################################################
 # GIF SEARCH ROUTE
